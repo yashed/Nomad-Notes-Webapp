fab55683f55f10da7308fa365d195c21415c1539
Terance Edmonds <teranceedmonds20@gmail.com>
2023-09-11 15:21:50 +0530

    user profile update

--- src/layout/navbar/index.jsx
+++ src/layout/navbar/index.jsx
@@ -5,14 +5,17 @@ import SignInModal from '../../modals/signin';
 import SignUpModal from '../../modals/signup';
 import axiosInstance from '../../utils/axios-instance';
 import { NotificationManager } from 'react-notifications';
+import EditUserModal from '../../modals/edit-user';
 
 export default function Navbar() {
     const location = useLocation();
-    const [user, setUser] = React.useState(null);
+    const [user, setUser] = React.useState({});
     const [isAdmin, setIsAdmin] = React.useState(false);
     const [openMenu, setOpenMenu] = React.useState(false);
+    const [openProfileMenu, setOpenProfileMenu] = React.useState(false);
     const [openSignInModal, setOpenSignInModal] = React.useState(false);
     const [openSignUpModal, setOpenSignUpModal] = React.useState(false);
+    const [openEditModal, setOpenEditModal] = React.useState(false);
 
     React.useEffect(() => {
         setUserSetting();
@@ -20,13 +23,26 @@ export default function Navbar() {
 
     const setUserSetting = () => {
         const user = sessionStorage.getItem('logged-user');
-        const admin = sessionStorage.getItem('is-admin');
 
-        setUser(user);
-        setIsAdmin(!!admin);
+        if (user)
+            axiosInstance
+                .get(`/users/${user}`)
+                .then((result) => {
+                    if (result?.data?.status === 'success') {
+                        setUser(result.data?.data);
+                        setIsAdmin(result.data?.data?.role == 'ADMIN');
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    NotificationManager.error(
+                        error?.response?.data?.message || error.message || 'Error message'
+                    );
+                });
     };
 
     const onSignedIn = () => {
+        setOpenEditModal(false);
         setOpenSignUpModal(false);
         setOpenSignInModal(false);
         setOpenMenu(false);
@@ -35,15 +51,37 @@ export default function Navbar() {
     };
 
     const onOpenSignIn = () => {
+        setOpenEditModal(false);
         setOpenSignUpModal(false);
         setOpenSignInModal(true);
         setOpenMenu(false);
+        setOpenProfileMenu(false);
     };
 
     const onOpenSignUp = () => {
+        setOpenEditModal(false);
         setOpenSignInModal(false);
         setOpenSignUpModal(true);
         setOpenMenu(false);
+        setOpenProfileMenu(false);
+    };
+
+    const onOpenEditProfile = () => {
+        setOpenSignUpModal(false);
+        setOpenSignInModal(false);
+        setOpenEditModal(true);
+        setOpenMenu(false);
+        setOpenProfileMenu(false);
+    };
+
+    const onEdited = () => {
+        setOpenEditModal(false);
+        setOpenProfileMenu(false);
+    };
+
+    const onSettingItem = () => {
+        setOpenMenu(false);
+        setOpenProfileMenu(false);
     };
 
     const onSignOut = () => {
@@ -123,28 +161,101 @@ export default function Navbar() {
                             </Link>
                         </li>
                         <li>
-                            {user ? (
-                                <Link
-                                    onClick={() => setOpenMenu(false)}
-                                    to="/story/create"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                    Create Story
-                                </Link>
-                            ) : (
-                                <span
-                                    onClick={() => openSignInModal()}
-                                    className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                    Create Story
-                                </span>
-                            )}
-                        </li>
-                        <li>
-                            {user ? (
-                                <span
-                                    onClick={onSignOut}
-                                    className="cursor-pointer block py-2 pl-3 pr-4 rounded md:border-0  md:p-0 text-red-500">
-                                    Logout
-                                </span>
+                            {user?._id ? (
+                                <div className="relative">
+                                    <button
+                                        onClick={() => setOpenProfileMenu(!openProfileMenu)}
+                                        id="dropdownNavbarLink"
+                                        data-dropdown-toggle="dropdownNavbar"
+                                        className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                                        <div className="flex items-center gap-2">
+                                            {user?.image ? (
+                                                <img
+                                                    className="w-8 h-8 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                                                    src={user?.image}
+                                                    alt="Bordered avatar"
+                                                />
+                                            ) : (
+                                                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 p-1 rounded-full ring-2 dark:bg-gray-600">
+                                                    <svg
+                                                        className="absolute w-10 h-10 text-gray-400 -left-1"
+                                                        fill="currentColor"
+                                                        viewBox="0 0 20 20"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                        <path
+                                                            fill-rule="evenodd"
+                                                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                                            clip-rule="evenodd"></path>
+                                                    </svg>
+                                                </div>
+                                            )}
+                                        </div>
+                                        <svg
+                                            className="w-2.5 h-2.5 ml-2.5"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 10 6">
+                                            <path
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="m1 1 4 4 4-4"
+                                            />
+                                        </svg>
+                                    </button>
+                                    <div
+                                        id="dropdownNavbar"
+                                        className={`${
+                                            !openProfileMenu && 'hidden'
+                                        } top-10 right-0 md:absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
+                                        <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                                            <div>{user?.name}</div>
+                                            <div className="font-medium truncate">
+                                                {user?.email}
+                                            </div>
+                                        </div>
+                                        <ul
+                                            className="w-full py-2 text-sm text-gray-700 dark:text-gray-400"
+                                            aria-labelledby="dropdownLargeButton">
+                                            <li>
+                                                <span
+                                                    onClick={onOpenEditProfile}
+                                                    className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    Profile
+                                                </span>
+                                            </li>
+
+                                            <li>
+                                                <Link
+                                                    onClick={onSettingItem}
+                                                    to="/story/create"
+                                                    className="text-inherit block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    Create Story
+                                                </Link>
+                                            </li>
+
+                                            {isAdmin && (
+                                                <li>
+                                                    <Link
+                                                        onClick={onSettingItem}
+                                                        to="/admin"
+                                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                        Dashboard
+                                                    </Link>
+                                                </li>
+                                            )}
+                                        </ul>
+                                        <div className="py-1">
+                                            <span
+                                                onClick={onSignOut}
+                                                className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-500">
+                                                Logout
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
                             ) : (
                                 <span
                                     onClick={onOpenSignIn}
@@ -153,16 +264,6 @@ export default function Navbar() {
                                 </span>
                             )}
                         </li>
-                        {isAdmin && (
-                            <li>
-                                <Link
-                                    to="/admin"
-                                    onClick={() => setOpenMenu(false)}
-                                    className="max-w-fit p-1 rounded md:border-0 bg-blue-500 text-white block ">
-                                    Dashboard
-                                </Link>
-                            </li>
-                        )}
                     </ul>
                 </div>
             </div>
@@ -188,6 +289,9 @@ export default function Navbar() {
                     onSignIn={onOpenSignIn}
                     setOpenModal={setOpenSignUpModal}
                 />
+            </PopUp>
+            <PopUp title={'Profile'} openModal={openEditModal} setOpenModal={setOpenEditModal}>
+                <EditUserModal data={user} onSuccess={onEdited} />
             </PopUp>
         </nav>
     );
